bfe937d25dcd30a41ac58a2016b93063f5d02aac
buildkernel1 <akunsatu270819@gmai.com>
2026-02-22 06:16:03 +0000

    sched/fair: fix C99 declaration-after-statement errors
    Move local variable declarations to the start of blocks in
    fair.c to comply with GNU89 style and avoid Clang errors:
    
      error: mixing declarations and code is a C99 extension
      [-Werror,-Wdeclaration-after-statement]
    
    This change ensures compatibility with Clang builds without
    relying solely on -Wno-declaration-after-statement flags.
    
    Signed-off-by: buildkernel1 <akunsatu270819@gmai.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6003,6 +6003,8 @@ static unsigned long cpu_util_without(int cpu, struct task_struct *p)
 {
 	unsigned int util;
 
+	struct cfs_rq *cfs_rq;
+	
 #ifdef CONFIG_SCHED_WALT
 	/*
 	 * WALT does not decay idle tasks in the same manner
@@ -6022,8 +6024,7 @@ static unsigned long cpu_util_without(int cpu, struct task_struct *p)
 #ifdef CONFIG_SCHED_WALT
 	util = max_t(long, cpu_util(cpu) - task_util(p), 0);
 #else
-	struct cfs_rq *cfs_rq;
-
+	
 	cfs_rq = &cpu_rq(cpu)->cfs;
 	util = READ_ONCE(cfs_rq->avg.util_avg);
 
