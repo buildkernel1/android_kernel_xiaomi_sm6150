3dbb17654c3cfc4beb5d6edcf7a3d82ccb937b18
buildkernel1 <akunsatu270819@gmai.com>
2026-02-23 06:53:52 +0000

    efi: libstub: add strrchr for EFI stub
    - Added strrchr() implementation to libstub/string.c.
    - Wrapped with #ifdef CONFIG_EFI_STUB to avoid duplicate symbols.
    - Provides __efistub_strrchr required by lib-fdt_ro.stub.o.
    - Resolves undefined symbol error during vmlinux link.
    
    Signed-off-by: buildkernel1 <akunsatu270819@gmai.com>

--- drivers/firmware/efi/libstub/string.c
+++ drivers/firmware/efi/libstub/string.c
@@ -9,50 +9,56 @@
 #include <linux/types.h>
 #include <linux/string.h>
 
+#ifdef CONFIG_EFI_STUB
+
+#ifndef __HAVE_ARCH_STRRCHR
+char *strrchr(const char *s, int c)
+{
+    const char *p = s + strlen(s);
+    do {
+        if (*p == (char)c)
+            return (char *)p;
+    } while (--p >= s);
+    return NULL;
+}
+#endif
+
 #ifndef __HAVE_ARCH_STRSTR
-/**
- * strstr - Find the first substring in a %NUL terminated string
- * @s1: The string to be searched
- * @s2: The string to search for
- */
 char *strstr(const char *s1, const char *s2)
 {
-	size_t l1, l2;
+    size_t l1, l2;
 
-	l2 = strlen(s2);
-	if (!l2)
-		return (char *)s1;
-	l1 = strlen(s1);
-	while (l1 >= l2) {
-		l1--;
-		if (!memcmp(s1, s2, l2))
-			return (char *)s1;
-		s1++;
-	}
-	return NULL;
+    l2 = strlen(s2);
+    if (!l2)
+        return (char *)s1;
+    l1 = strlen(s1);
+    while (l1 >= l2) {
+        l1--;
+        if (!memcmp(s1, s2, l2))
+            return (char *)s1;
+        s1++;
+    }
+    return NULL;
 }
 #endif
 
 #ifndef __HAVE_ARCH_STRNCMP
-/**
- * strncmp - Compare two length-limited strings
- * @cs: One string
- * @ct: Another string
- * @count: The maximum number of bytes to compare
- */
 int strncmp(const char *cs, const char *ct, size_t count)
 {
-	unsigned char c1, c2;
+    unsigned char c1, c2;
 
-	while (count) {
-		c1 = *cs++;
-		c2 = *ct++;
-		if (c1 != c2)
-			return c1 < c2 ? -1 : 1;
-		if (!c1)
-			break;
-		count--;
-	}
-	return 0;
+    while (count) {
+        c1 = *cs++;
+        c2 = *ct++;
+        if (c1 != c2)
+            return c1 < c2 ? -1 : 1;
+        if (!c1)
+            break;
+        count--;
+    }
+    return 0;
 }
 #endif
+
+#endif /* CONFIG_EFI_STUB */
+
